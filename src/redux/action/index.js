fbced9cdc832a3aba16d046c80e5c7bfe6859cf5
mehuljetani <mehuljetani23@gmail.com>
2022-07-21 10:51:34 +0530

    commit on day

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,27 +1,9 @@
-import {DELETE_USER, GET_USER, UPDATE_USER} from './type';
+import {GET_USER} from './type';
 
 export const getUser = data => {
   return dispatch => {
     dispatch({
       type: GET_USER,
-      payload: data,
-    });
-  };
-};
-
-export const deleteUser = id => {
-  return dispatch => {
-    dispatch({
-      type: DELETE_USER,
-      payload: id,
-    });
-  };
-};
-
-export const updateUser = data => {
-  return dispatch => {
-    dispatch({
-      type: UPDATE_USER,
       payload: data,
     });
   };
